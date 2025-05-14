382ccc2a8257f3ef0b5dfef7536cac85cc14ef31
Jan Zdenek <xzdene01@vutbr.cz>
2025-05-14 19:14:46 +0200

    fixed merge conflict

--- wrappers/fastopic_wrapper.py
+++ wrappers/fastopic_wrapper.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import numpy as np
 import logging
 import argparse
 import stopwordsiso as stopwords
@@ -35,7 +36,7 @@ class FASTopicWrapper(WrapperBase):
             self.model = FASTopic.from_pretrained(args.load_path, device=args.device)
         else:
             logging.info("Training model from scratch.")
-        
+
             # Create Preprocessor that will be later used to convert data into BoW representation
             stop_words = get_stop_words(args.stopwords) + list(stopwords.stopwords("cs"))
             tokenizer = CzechLemmatizedTokenizer(stopwords=stop_words, cache_dir=args.cache_dir)
@@ -48,7 +49,7 @@ class FASTopicWrapper(WrapperBase):
                     embeddings = embeddings[self.doc_idxs]
                     doc_embedder = StubEncoder(embeddings)
                     logging.info(f"Loaded {embeddings.shape[0]} embeddings from {args.embes_path} and initialized a stub encoder.")
-                
+
                 # Pre-compute embeddings and save them, then use stub encoder with them
                 else:
                     embedder = BasicSentenceEmbedder(model=self.embedder_name,
@@ -57,10 +58,16 @@ class FASTopicWrapper(WrapperBase):
                                                      verbose=args.verbose,
                                                      torch_dtype=torch_dtype)
                     embeddings = embedder.encode(self.all_docs)
+
+                    # check and cast to float32
+                    if embeddings.dtype != np.float32:
+                        logging.info(f"Converting embeddings from {embeddings.dtype} to float32")
+                        embeddings = embeddings.astype(np.float32)
+
                     save_h5(args.embes_path, embeddings)
-                    doc_embedder = StubEncoder(embeddings[self.doc_idxs])
+                    doc_embedder = StubEncoder(embeddings[self.doc_idxs.cpu()])
                     logging.info(f"Computed and saved {embeddings.shape[0]} embeddings to {args.embes_path}.")
-            
+
             # Compute embeddings on the fly
             else:
                 doc_embedder = BasicSentenceEmbedder(model=self.embedder_name,
@@ -81,11 +88,12 @@ class FASTopicWrapper(WrapperBase):
                              low_memory=args.batch_size is not None,
                              low_memory_batch_size=args.batch_size,
                              verbose=args.verbose,
-                             normalize_embeddings=args.norm_embes)
-            
+                             normalize_embeddings=args.norm_embes,
+                             DT_alpha=10.0, TW_alpha=10.0)
+
             model.fit_transform(docs, epochs=args.epochs, learning_rate=args.lr)
             self.model = model
-        
+
         # Save model
         if args.save_path:
             try:
@@ -98,29 +106,16 @@ class FASTopicWrapper(WrapperBase):
             logging.info(f"Model saved to {args.save_path}.")
         elif not args.load_path:
             logging.warning("Model was not saved, use --save_path to save the model.")
-        
+
         logging.info(f"Model initialized with {self.args.num_topics} topics.")
-    
-    # @property
-    # def topic_diversity(self):
-    #     top_words = self.model.get_top_words(self.args.num_top_words, verbose=False)
-    #     return topic_diversity._diversity(top_words)
-    
-    # @property
-    # def topic_coherence(self):
-    #     texts = self.all_docs
-    #     vocab = self.model.vocab
-    #     top_words = self.model.get_top_words(self.args.num_top_words, verbose=False)
-    #     coherence = topic_coherence._coherence(texts, vocab, top_words)
-    #     return coherence
-    
+
     def visualize_hierarchy(self, save_path=None):
         fig = self.model.visualize_topic_hierarchy()
         if save_path:
             fig.write_image(save_path)
         else:
             fig.show()
-    
+
     def visualize_weights(self, save_path=None):
         fig = self.model.visualize_topic_weights()
         if save_path:
