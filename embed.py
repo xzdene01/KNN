117c71613a1b67c4cd2596f25077b15b8bd0ce2a
Jan Zdenek <xzdene01@vutbr.cz>
2025-05-15 10:39:22 +0200

    rewritten embedding to work with bge

--- embed.py
+++ embed.py
@@ -2,12 +2,14 @@ import os
 os.environ["HF_HOME"] = "./models"
 
 import torch
+import numpy as np
 from sentence_transformers import SentenceTransformer
 
 from utils.dataset import load_docs, save_h5
 
 base_path = "data/reduced_dataset"
 model_name = "BAAI/bge-multilingual-gemma2"
+
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -19,18 +21,23 @@ def main():
     print(f"Loaded {len(docs)} documents from {docs_path}")
 
     # Load model
-    model = SentenceTransformer(model_name, device=device)
+    model = SentenceTransformer(model_name, device=device, model_kwargs={"torch_dtype": torch.float16})
     print(f"Loaded model: {model_name}")
 
     # Set max token length
     # model.max_seq_length = 1024
 
     # Encode documents
-    embeddings = model.encode(docs, show_progress_bar=True, normalize_embeddings=False)
+    embeddings = model.encode(docs, batch_size=1, show_progress_bar=True, normalize_embeddings=True)
     print(f"Encoded {len(docs)} documents")
 
+    # check and cast to float32
+    if embeddings.dtype != np.float32:
+        print(f"Converting embeddings from {embeddings.dtype} to float32")
+        embeddings = embeddings.astype(np.float32)
+
     # Save embeddings to HDF5 file
-    h5_path = base_path + "_" + model_name + ".h5"
+    h5_path = base_path + "_" + model_name + "_norm" + ".h5"
     save_h5(h5_path, embeddings)
 
     return
